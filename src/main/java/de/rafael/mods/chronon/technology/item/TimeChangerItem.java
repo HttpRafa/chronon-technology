fe952c50833f12e7bac64ff0d2d1a771507123bf
HttpRafa <rafael@kienitz.cloud>
2023-08-23 18:15:19 +0200

    KA

--- src/main/java/de/rafael/mods/chronon/technology/item/TimeChangerItem.java
+++ src/main/java/de/rafael/mods/chronon/technology/item/TimeChangerItem.java
@@ -3,9 +3,14 @@ package de.rafael.mods.chronon.technology.item;
 import de.rafael.mods.chronon.technology.config.AcceleratorConfig;
 import de.rafael.mods.chronon.technology.item.abstracted.ChrononStorageItem;
 import de.rafael.mods.chronon.technology.registry.ModItems;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
 public class TimeChangerItem extends ChrononStorageItem {
@@ -19,6 +24,13 @@ public class TimeChangerItem extends ChrononStorageItem {
         ItemStack itemStack = new ItemStack(item);
         item.setChronons(itemStack, item.getMaxStorageSize());
         return itemStack;
+    }
+
+    @Override
+    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, Player player, InteractionHand interactionHand) {
+        if(!level.isClientSide()) return super.use(level, player, interactionHand);
+        player.sendSystemMessage(Component.literal("Client text"));
+        return super.use(level, player, interactionHand);
     }
 
 }
